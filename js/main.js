659f68faf5d37e2314997c6e4d967b02b2e08e52
RSO77 <ostapchenko.roma@gmail.com>
2018-07-15 21:07:38 +0300

    15

--- js/main.js
+++ js/main.js
@@ -1,16 +1,34 @@
 $(function () {
 
+    //scrol
+    $('.bot-anim').bind('click', function (e) {
+        e.preventDefault();
+        if ($(this).hasClass('prevent')) {
+            return false;
+        }
+        var target = this.hash,
+            $target = $(target);
+
+        var top = typeof  $target.offset() != 'undefined' ? $target.offset().top : 0
+
+        $('html, body').stop().animate({
+            'scrollTop': top
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+
     //slider
     var swiper = new Swiper('.swiper-sl1', {
         slidesPerView: 1,
         speed: 700,
         spaceBetween: 0,
         loop: true,
-        // autoplay: {
-        //     delay: 7000,
-        //     disableOnInteraction: false,
-        // },
-        // effect : 'fade',
+        autoplay: {
+            delay: 7000,
+            disableOnInteraction: false,
+        },
+        effect : 'fade',
         pagination: {
             el: '.sp1',
             clickable: true,
@@ -22,17 +40,17 @@ $(function () {
 
     //slider2
     var swiper2 = new Swiper('.swiper-sl2', {
-        slidesPerView: 3,
+        slidesPerView: 2.8,
         speed: 700,
         centeredSlides: true,
         spaceBetween: 150,
         loop: true,
         slidesPerGroup: 1,
         loopFillGroupWithBlank: true,
-        // autoplay: {
-        //     delay: 7000,
-        //     disableOnInteraction: false,
-        // },
+        autoplay: {
+            delay: 7000,
+            disableOnInteraction: false,
+        },
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
@@ -42,7 +60,7 @@ $(function () {
                 slidesPerView: 1,
                 spaceBetween: 0
             },
-            1024: {
+            1042: {
                 slidesPerView: 1.4,
                 spaceBetween: 70
             }
@@ -86,32 +104,36 @@ $(function () {
         $('.s1-plus-up').toggleClass('s1-plus-up-show');
         $('.s1-pop-up').toggleClass('s1-pop-up-show');
     });
-    
-    //swipe header
-    // $(window).scroll(function () {
-    //     console.log($(window).scrollTop());
-    //     if ($(window).scrollTop() > 70) {
-    //         $('.header').addClass('header-swipe')
-    //     }
-    //     else{$('.header').removeClass('header-swipe')}
-    // })
 
-    //scrol
-    // $('.detailed').bind('click', function (e) {
-    //     e.preventDefault();
-    //     if ($(this).hasClass('prevent')) {
-    //         return false;
-    //     }
-    //     var target = this.hash,
-    //         $target = $(target);
-    //
-    //     var top = typeof  $target.offset() != 'undefined' ? $target.offset().top : 0
-    //
-    //     $('html, body').stop().animate({
-    //         'scrollTop': top
-    //     }, 500, 'swing', function () {
-    //         window.location.hash = target;
-    //     });
-    // });
-    
+    //переключатель s2
+    $('.command-b').click(function () {
+        $('.command-b').addClass('activ-s2-b');  $('.command').addClass('activ-s2');
+        $('.history-b, .achievements-b').removeClass('activ-s2-b');   $('.history,.achievements').removeClass('activ-s2');
+    });
+    $('.history-b').click(function () {
+        $('.history-b').addClass('activ-s2-b'); $('.history').addClass('activ-s2');
+        $('.command-b,.achievements-b').removeClass('activ-s2-b'); $('.command,.achievements').removeClass('activ-s2');
+    });
+    $('.achievements-b').click(function () {
+        $('.achievements-b').addClass('activ-s2-b'); $('.achievements').addClass('activ-s2');
+        $('.command-b,.history-b').removeClass('activ-s2-b');$('.command,.history').removeClass('activ-s2');
+    });
+    //переключатель s3
+    $('.Curbstones-b').click(function () {
+        $('.Curbstones-b').addClass('activ-s3-b');  $('.Curbstones').addClass('activ-s3');
+        $('.tables, .racks, .Coffee-tables').removeClass('activ-s3');   $('.Coffee-tables-b,.tables-b,.racks-b').removeClass('activ-s3-b');
+    });
+    $('.Coffee-tables-b').click(function () {
+        $('.Coffee-tables').addClass('activ-s3'); $('.Coffee-tables-b').addClass('activ-s3-b');
+        $('.Curbstones-b,.tables-b,.racks-b').removeClass('activ-s3-b'); $('.Curbstones,.racks,.tables').removeClass('activ-s3');
+    });
+    $('.racks-b').click(function () {
+        $('.racks').addClass('activ-s3'); $('.racks-b').addClass('activ-s3-b');
+        $('.Curbstones-b,.tables-b,.Coffee-tables-b').removeClass('activ-s3-b');$('.Curbstones,.tables,.Coffee-tables').removeClass('activ-s3');
+    });
+    $('.tables-b').click(function () {
+        $('.tables').addClass('activ-s3'); $('.tables-b').addClass('activ-s3-b');
+        $('.Curbstones-b,.racks-b,.Coffee-tables-b').removeClass('activ-s3-b');$('.Curbstones,.racks,.Coffee-tables').removeClass('activ-s3');
+    });
+
 });
